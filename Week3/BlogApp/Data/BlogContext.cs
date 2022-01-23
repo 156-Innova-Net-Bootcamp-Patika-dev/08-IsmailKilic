510af7ffcb2a650e480f490792dc55a08bb4f468
Ismail Kılıç <ismail000728@gmail.com>
2022-01-23 21:46:39 +0300

    Data layer and repository pattern added.

---
+++ Week3/BlogApp/Data/BlogContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Week3/BlogApp/Data/EfCore/EfCoreUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace Data.EfCore
+{
+    public class EfCoreUserRepository : EfCoreRepository<User, BlogContext>
+    {
+        private BlogContext _blogContext;
+        public EfCoreUserRepository(BlogContext context) : base(context)
+        {
+            _blogContext = context;
+        }
+
+        public User GetByEmail(string email)
+        {
+            var user = _blogContext.Users
+                .Where(x => x.Email == email).FirstOrDefault();
+            return user;
+        }
+    }
+}
