16ff0b4d5cd13a06a4f5bb14eff106fd1fc3613a
Ismail Kılıç <ismail000728@gmail.com>
2022-02-15 15:18:43 +0300

    Create payment created event.

---
+++ FinalProject/Server/Application/Features/Events/PaymentCreatedConsumer.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using MassTransit;
+using MessageContracts.Events;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Features.Events
+{
+    public class PaymentCreatedConsumer : IConsumer<PaymentCreated>
+    {
+        ILogger<PaymentCreatedConsumer> _logger;
+
+        public PaymentCreatedConsumer(ILogger<PaymentCreatedConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<PaymentCreated> context)
+        {
+            _logger.LogInformation("Value: {Value}", context.Message.InvoiceId);
+        }
+    }
+}
