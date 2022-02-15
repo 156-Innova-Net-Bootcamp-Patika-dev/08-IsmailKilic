7e377191dd7f391954139062decb074668e0adb0
Ismail Kılıç <ismail000728@gmail.com>
2022-02-15 16:50:50 +0300

    Implement repository pattern for mongodb.

---
+++ FinalProject/Server/PaymentAPI/Data/BsonCollectionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PaymentAPI.Data
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class BsonCollectionAttribute : Attribute
+    {
+        public string CollectionName { get; set; }
+
+        public BsonCollectionAttribute(string collectionName)
+        {
+            CollectionName = collectionName;
+        }
+    }
+}
---
+++ FinalProject/Server/PaymentAPI/Data/MongoRepository.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using PaymentAPI.Models;
+
+namespace PaymentAPI.Data
+{
+    public class MongoRepository<TDocument> : IMongoRepository<TDocument>
+        where TDocument : IEntity
+    {
+        private readonly IMongoCollection<TDocument> _collection;
+        private readonly IMongoDbSettings _settings;
+
+        public MongoRepository(IMongoDbSettings settings)
+        {
+            _settings = settings;
+            var database = new MongoClient(_settings.ConnectionString).GetDatabase(_settings.Database);
+            _collection = database.GetCollection<TDocument>(GetCollectionName(typeof(TDocument)));
+        }
+
+        private protected string GetCollectionName(Type documentType)
+        {
+            return ((BsonCollectionAttribute)documentType.GetCustomAttributes(
+                    typeof(BsonCollectionAttribute),
+                    true)
+                .FirstOrDefault())?.CollectionName;
+        }
+        public IQueryable<TDocument> AsQueryable()
+        {
+            return _collection.AsQueryable();
+        }
+
+        public Task DeleteByIdAsync(string id)
+        {
+            return Task.Run(() =>
+            {
+                var objectId = new ObjectId(id);
+                var filter = Builders<TDocument>.Filter.Where(doc => doc.Id == id);
+                _collection.FindOneAndDeleteAsync(filter);
+            });
+        }
+
+        public Task DeleteManyAsync(Expression<Func<TDocument, bool>> filterExpression)
+        {
+            return Task.Run(() => _collection.DeleteManyAsync(filterExpression));
+        }
+
+        public Task DeleteOneAsync(Expression<Func<TDocument, bool>> filterExpression)
+        {
+            return Task.Run(() => _collection.FindOneAndDeleteAsync(filterExpression));
+        }
+
+        public virtual IEnumerable<TDocument> FilterBy(
+            Expression<Func<TDocument, bool>> filterExpression)
+        {
+            return _collection.Find(filterExpression).ToEnumerable();
+        }
+
+        public virtual IEnumerable<TProjected> FilterBy<TProjected>(
+            Expression<Func<TDocument, bool>> filterExpression,
+            Expression<Func<TDocument, TProjected>> projectionExpression)
+        {
+            return _collection.Find(filterExpression).Project(projectionExpression).ToEnumerable();
+        }
+
+        public virtual Task<TDocument> FindByIdAsync(string id)
+        {
+            return Task.Run(() =>
+            {
+                var objectId = new ObjectId(id);
+                var filter = Builders<TDocument>.Filter.Where(doc => doc.Id == id);
+                return _collection.Find(filter).SingleOrDefaultAsync();
+            });
+        }
+
+        public virtual Task<TDocument> FindOneAsync(Expression<Func<TDocument, bool>> filterExpression)
+        {
+            return Task.Run(() => _collection.Find(filterExpression).FirstOrDefaultAsync());
+        }
+
+        public virtual async Task InsertManyAsync(ICollection<TDocument> documents)
+        {
+            await _collection.InsertManyAsync(documents);
+        }
+
+        public virtual Task InsertOneAsync(TDocument document)
+        {
+            return Task.Run(() => _collection.InsertOneAsync(document));
+        }
+
+        public virtual async Task ReplaceOneAsync(TDocument document)
+        {
+            var filter = Builders<TDocument>.Filter.Eq(doc => doc.Id, document.Id);
+            await _collection.FindOneAndReplaceAsync(filter, document);
+        }
+    }
+}
