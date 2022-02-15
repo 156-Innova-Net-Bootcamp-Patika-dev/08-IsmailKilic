bcb64373bbb9b7fac345a056e9fa6278106f7a2a
Ismail Kılıç <ismail000728@gmail.com>
2022-02-15 15:20:12 +0300

    Add publishing event for payment api.

---
+++ FinalProject/Server/PaymentAPI/Controllers/PaymentsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using MassTransit;
+using MessageContracts.Events;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IPublishEndpoint publishEndpoint;
+
+        public PaymentsController(IPublishEndpoint publishEndpoint)
+        {
+            this.publishEndpoint = publishEndpoint;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(int value)
+        {
+            await publishEndpoint.Publish<PaymentCreated>(new PaymentCreated
+            {
+                InvoiceId = value
+            });
+
+            return Ok();
+        }
+    }
+}
