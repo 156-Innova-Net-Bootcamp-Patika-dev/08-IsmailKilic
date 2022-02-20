815cd46ef5d1086b31bc1e7acc94ee1c3e90fd3f
Ismail Kılıç <ismail000728@gmail.com>
2022-02-20 14:58:50 +0300

    Add get apartments tests.

---
+++ FinalProject/Server/tests/Management.Tests/Queries/GetApartmentTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Features.Queries.GetApartment;
+using Application.Interfaces.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace Management.Tests.Queries
+{
+    public class GetApartmentTests
+    {
+        private readonly Mock<IAparmentRepository> mockRepo;
+        private readonly Mock<IMapper> mockMapper;
+
+        public GetApartmentTests()
+        {
+            mockRepo = new Mock<IAparmentRepository>();
+            mockMapper = new Mock<IMapper>();
+        }
+
+        [Fact]
+        public async Task ShouldReturnApartment()
+        {
+            // arrange
+            mockRepo.Setup(x => x.Get(It.IsAny<Expression<Func<Apartment, bool>>>(), It.IsAny<Expression<Func<Apartment, object>>[]>()))
+                .Returns(new Apartment { Block = 'A', Floor = 1 });
+
+            mockMapper.Setup(x => x.Map<GetApartmentResponse>(It.IsAny<Apartment>()))
+               .Returns(new GetApartmentResponse { Block = 'A', Floor = 1 });
+
+            var handler = new GetApartmentHandler(mockRepo.Object, mockMapper.Object);
+
+            // act
+            var result = await handler.Handle(new GetApartmentQuery { Id = 1}, CancellationToken.None);
+
+            // assert
+            result.Block.ShouldBe('A');
+            result.Floor.ShouldBe(1);
+            mockRepo.Verify(x => x.Get(It.IsAny<Expression<Func<Apartment, bool>>>(), It.IsAny<Expression<Func<Apartment, object>>[]>()), Times.Once);
+        }
+    }
+}
---
+++ FinalProject/Server/tests/Management.Tests/Queries/GetApartmentsTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Features.Queries.GetApartment;
+using Application.Features.Queries.GetApartments;
+using Application.Interfaces.Cache;
+using Application.Interfaces.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using Management.Tests.Mocks.Register;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace Management.Tests.Queries
+{
+    public class GetApartmentsTests
+    {
+        private readonly Mock<IAparmentRepository> mockRepo;
+        private readonly Mock<IMapper> mockMapper;
+        private readonly Mock<ICacheService> mockCache;
+
+        public GetApartmentsTests()
+        {
+            mockCache = MockCacheService.GetCacheService();
+            mockRepo = new Mock<IAparmentRepository>();
+            mockMapper = new Mock<IMapper>();
+        }
+
+        [Fact]
+        public async Task ShouldReturnAllApartments()
+        {
+            // arrange
+            mockRepo.Setup(x => x.GetList(It.IsAny<Expression<Func<Apartment, bool>>>(), It.IsAny<Expression<Func<Apartment, object>>[]>()))
+                .Returns(new List<Apartment>
+                {
+                    new Apartment { Id = 1,}
+                });
+
+            mockMapper.Setup(x => x.Map<List<GetApartmentsResponse>>(It.IsAny<List<Apartment>>()))
+               .Returns(new List<GetApartmentsResponse>
+               {
+                   new GetApartmentsResponse { Id = 1,}
+               });
+
+            var handler = new GetApartmentsHandler(mockRepo.Object, mockMapper.Object, mockCache.Object);
+
+            // act
+            var result = await handler.Handle(new GetApartmentsQuery {  }, CancellationToken.None);
+
+            // assert
+            result.Count.ShouldBe(1);
+            mockRepo.Verify(x => x.GetList(It.IsAny<Expression<Func<Apartment, bool>>>(), It.IsAny<Expression<Func<Apartment, object>>[]>()),
+                Times.Once);
+        }
+    }
+}
