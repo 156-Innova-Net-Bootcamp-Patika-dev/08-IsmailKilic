abf805dfeb49f86da2f0aae8738e0bf03b96e908
Ismail Kılıç <ismail000728@gmail.com>
2022-02-20 15:27:42 +0300

    Implement user query tests.

---
+++ FinalProject/Server/tests/Management.Tests/Queries/GetAuthenticatedUserTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Exceptions;
+using Application.Features.Queries.GetAuthenticatedUser;
+using AutoMapper;
+using Domain.Entities;
+using Management.Tests.Mocks.Register;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace Management.Tests.Queries
+{
+    public class GetAuthenticatedUserTests
+    {
+        private readonly Mock<UserManager<ApplicationUser>> mockRepo;
+        private readonly Mock<IMapper> mockMapper;
+
+        public GetAuthenticatedUserTests()
+        {
+            mockRepo = RegisterUserManager.GetRegisterUserManager();
+            mockMapper = new Mock<IMapper>();
+        }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundErrorIfUserNotFound()
+        {
+            // arrange
+            mockRepo.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(value: null);
+
+            var handler = new GetAuthenticatedUserHandler(mockRepo.Object, mockMapper.Object);
+
+            // act
+            Task act() => handler.Handle(new GetAuthenticatedUserQuery { Id = "123" }, CancellationToken.None);
+
+            var exception = await Assert.ThrowsAsync<NotFoundException>(act);
+
+            // assert
+            Assert.Equal("Kullanıcı bulunamadı", exception.Message);
+            mockRepo.Verify(x => x.FindByIdAsync(It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task ShouldReturnAuthenticatedUser()
+        {
+            // arrange
+            mockRepo.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(new ApplicationUser { });
+            mockRepo.Setup(x => x.GetRolesAsync(It.IsAny<ApplicationUser>())).ReturnsAsync(new List<string> { "User" });
+
+            mockMapper.Setup(x => x.Map<GetAuthenticatedUserResponse>(It.IsAny<ApplicationUser>()))
+               .Returns(new GetAuthenticatedUserResponse { Email = "test@test.com", Roles = new List<string> { "User" } });
+
+            var handler = new GetAuthenticatedUserHandler(mockRepo.Object, mockMapper.Object);
+
+            // act
+            var result = await handler.Handle(new GetAuthenticatedUserQuery { Id = "123" }, CancellationToken.None);
+
+            // assert
+            mockRepo.Verify(x => x.FindByIdAsync(It.IsAny<string>()), Times.Once);
+            mockRepo.Verify(x => x.GetRolesAsync(It.IsAny<ApplicationUser>()), Times.Once);
+            result.Roles[0].ShouldBe("User");
+            result.Email.ShouldBe("test@test.com");
+        }
+
+    }
+}
---
+++ FinalProject/Server/tests/Management.Tests/Queries/GetUsersTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Features.Queries.GetUsers;
+using Application.Interfaces.Cache;
+using AutoMapper;
+using Domain.Entities;
+using Management.Tests.Mocks.Register;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace Management.Tests.Queries
+{
+    public class GetUsersTests
+    {
+        private readonly Mock<UserManager<ApplicationUser>> mockRepo;
+        private readonly Mock<IMapper> mockMapper;
+        private readonly Mock<ICacheService> mockCache;
+
+        public GetUsersTests()
+        {
+            mockRepo = RegisterUserManager.GetRegisterUserManager();
+            mockMapper = new Mock<IMapper>();
+            mockCache = MockCacheService.GetCacheService();
+        }
+
+        [Fact]
+        public async Task ShouldReturnAllUsers()
+        {
+            // arrange
+            var users = new List<ApplicationUser>
+            {
+                new ApplicationUser { Id = "1",  },
+                new ApplicationUser { Id = "2",  },
+            }.AsQueryable();
+            mockRepo.Setup(x => x.Users).Returns(users);
+
+            mockMapper.Setup(x => x.Map<List<GetUsersResponse>>(It.IsAny<List<ApplicationUser>>()))
+               .Returns(new List<GetUsersResponse>
+               {
+                   new GetUsersResponse {  Id = "1"},
+                   new GetUsersResponse {  Id = "2"},
+               });
+
+            var handler = new GetUsersHandler(mockRepo.Object, mockMapper.Object, mockCache.Object);
+
+            // act
+            var result = await handler.Handle(new GetUsersQuery { }, CancellationToken.None);
+
+            // assert
+            result.Count.ShouldBe(2);
+            result[0].Id.ShouldBe("1");
+            result[1].Id.ShouldBe("2");
+
+            mockRepo.Verify(x => x.Users, Times.Once);
+        }
+    }
+}
