48ebfd19adee758f535b9e6c4b919bb20bd8753d
Ismail Kılıç <ismail000728@gmail.com>
2022-02-22 13:19:13 +0300

    Create payment consumers tests.

---
+++ FinalProject/Server/tests/Payment.Tests/Events/InvoiceCreatedConsumerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using MassTransit;
+using MessageContracts.Events;
+using Moq;
+using PaymentAPI.Consumers;
+using PaymentAPI.Data;
+using PaymentAPI.Models;
+using Xunit;
+
+namespace Payment.Tests.Events
+{
+    public class InvoiceCreatedConsumerTests
+    {
+        private readonly Mock<IMongoRepository<Invoice>> mockRepo;
+
+        public InvoiceCreatedConsumerTests()
+        {
+            mockRepo = new Mock<IMongoRepository<Invoice>>();   
+        }
+
+        [Fact]
+        public async Task ShouldCreateInvoiceWhenEventPublished()
+        {
+            mockRepo.Setup(x => x.InsertOneAsync(It.IsAny<Invoice>()));
+
+            var consumer = new InvoiceCreatedConsumer(mockRepo.Object);
+
+            await consumer.Consume(Mock.Of<ConsumeContext<InvoiceCreated>>(x => x.Message.InvoiceId == 1));
+
+            mockRepo.Verify(x => x.InsertOneAsync(It.IsAny<Invoice>()), Times.Once);
+        }
+    }
+}
---
+++ FinalProject/Server/tests/Payment.Tests/Events/UserCreatedConsumerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransit;
+using MessageContracts.Events;
+using Moq;
+using PaymentAPI.Consumers;
+using PaymentAPI.Data;
+using PaymentAPI.Models;
+using Xunit;
+
+namespace Payment.Tests.Events
+{
+    public class UserCreatedConsumerTests
+    {
+        private readonly Mock<IMongoRepository<User>> mockRepo;
+
+        public UserCreatedConsumerTests()
+        {
+            mockRepo = new Mock<IMongoRepository<User>>();
+        }
+
+        [Fact]
+        public async Task ShouldCreateUserWhenEventPublished()
+        {
+            mockRepo.Setup(x => x.InsertOneAsync(It.IsAny<User>()));
+
+            var consumer = new UserCreatedConsumer(mockRepo.Object);
+
+            await consumer.Consume(Mock.Of<ConsumeContext<UserCreated>>(x => x.Message.UserId == "123"));
+
+            mockRepo.Verify(x => x.InsertOneAsync(It.IsAny<User>()), Times.Once);
+        }
+    }
+}
